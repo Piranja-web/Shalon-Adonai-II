978abac2fabe311f7d54a05ae8c73647a7ac2bbf
Piranja-web <gabriel_sspirangi@hotmail.com>
2023-05-19 20:18:34 -0300

    - Correção da navbar mobile; - Adição de footer responsivo.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
 let ul = document.querySelector('nav ul');
 
 function openMenu(){
-    ul.classList.add('open');
-}
+        ul.classList.add('open');
+    }
 
 function closeMenu(){
     ul.classList.remove('open');
 }
+
+ul.classList.remove('open')
